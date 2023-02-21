7ae9ffd2d0ca1163030ade165d6b6ed3e9a176e3
andersonpolanc <andersonpolanco@presidencia.gob.do>
2023-02-21 16:15:20 -0400

    Disponibilidades

--- Repositories/Impl/DisponibilidadesRepository.cs
+++ Repositories/Impl/DisponibilidadesRepository.cs
@@ -1,5 +1,7 @@
 ﻿using EdecanesV2.Data;
 using EdecanesV2.Models;
+using Microsoft.EntityFrameworkCore;
+using static EdecanesV2.Models.Horario;
 
 namespace EdecanesV2.Repositories.Impl
 {
@@ -11,6 +13,47 @@ namespace EdecanesV2.Repositories.Impl
         {
             _context = context;
         }
+
+        public IEnumerable<string> FechasNoDisponibles()
+        {
+            var recorridosProgramados = GetRecorridosProgramados();
+
+
+            //TODO
+
+            return null;
+        }
+
+        private List<KeyValuePair<DiaSemana, int>> GetCantidadHorariosPorDiaSemana()
+        {
+            List<KeyValuePair<DiaSemana, int>> horariosPorDia = new();
+
+            var h = (from horario in _context.Horarios
+                    group horario by horario.Dia into dias
+                    select new
+                    {
+                        Key = dias.Key,
+                        horarios = dias.Count()
+                    }).ToList();
+
+            foreach (var item in h)
+            {
+                horariosPorDia.Add(new KeyValuePair<DiaSemana, int>(item.Key, item.horarios));
+            }
+
+            return horariosPorDia;
+
+        }
+
+        private List<RecorridoHistorico> GetRecorridosProgramados()
+        {
+            return _context.RecorridosHistoricos.AsNoTracking()
+                .Include(r => r.Horario)
+                .Include(r => r.TipoRecorrido)
+                .Where(r => r.FechaVisita >= DateTime.Now).ToList();
+        }
+
+        private void 
 
 
     }
